092687926538b84c66945cb15993f412f2fb9f95
Sanskar Sharma <sankysaa20@gmail.com>
2023-10-15 00:03:01 +0530

    fourth commit

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -79,7 +79,7 @@ const Form = () => {
   };
 
   const login = async (values, onSubmitProps) => {
-    const loggedInResponse = await fetch("${import.meta.env.VITE_API_URL}/auth/login", {
+    const loggedInResponse = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
